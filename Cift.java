ead0c24f2b4caaf212d52dbfab471d8b71e27456
Berat Altındal <berat_altindal.97@hotmail.com>
2022-08-19 14:23:41 +0300

    add

---
+++ Cift.java
@@ -0,0 +1,34 @@
+
+public class Cift {
+    static boolean isFind(int[] arr, int value){
+        for(int i: arr){
+            if(i == value ){
+                return true;
+            }
+
+        }
+        return false;
+    }
+    public static void main(String[] args) {
+        int[] list = { 12, 3, 4, 5, 6, 3, 7, 10, 3, 5, 7, 4 };
+        int[] duplicate = new int[list.length];
+        int startIndex = 0;
+
+        for ( int i = 0; i < list.length; i++){
+            for( int j = 0; j < list.length; j++ ){
+                if((i != j) && (list[i] == list[j])){
+                    if(!isFind(duplicate, list[i])){
+
+                    duplicate[startIndex++] = list[i];
+                    }
+                    break;
+                }
+            }
+        }
+        
+        for (int value : duplicate){
+            if((value % 2 == 0) && (value != 0))
+            System.out.println(value);
+        }
+    }
+}
